9cd939a40c79c2c711599574a9b15cd7b60b9ed3
Developer866 <opeyemijoseph866@gmail.com>
2025-07-27 21:50:30 +0100

    added image folder and added the nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/HEro";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <div className="text-3xl text-indigo-300">App</div>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
   );
 };
 
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HEro() {
+  return (
+    <div>HEro</div>
+  )
+}
+
+export default HEro
