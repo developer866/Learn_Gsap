550642d25a99e746e105d2f09425cb66ee7a5065
Ayeni opeyemi [factorial] <66707128+developer866@users.noreply.github.com>
2025-07-28 11:44:28 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from "gsap/all";
 
-import Navbar from './components/Navbar.jsx'
+// import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import Cocktails from './components/Cocktails.jsx'
 import About from './components/About.jsx'
